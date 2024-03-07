54669debf55c188fd0d74bd28684df8823d241f1
Cesar Leva <cesarle1975@gmail.com>
2024-03-07 07:15:20 -0300

    Primer Commit

---
+++ part1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const mensaje = 'Hola mundo'
+  return (
+    <div className="App">
+      {mensaje + ' evaluacion JSX'}
+    </div>
+  );
+}
+
+export default App;
+ 
